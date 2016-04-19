331e32077e827e0be4aea786656f87e289ca7d72
ardyflora <eripflo@gmail.com>
2016-04-18 23:56:33 -0400

    Adding batting scorecard points without dismissal, need to clean the code later

--- fantasyCricScraper.py
+++ fantasyCricScraper.py
@@ -1,5 +1,5 @@
 from selenium import webdriver
-import time, math, random
+import time, math, random, lxml.html
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
@@ -57,4 +57,53 @@ for element in listOfElem:
 		Player['Points'] = total
 		total_list.append(Player)
 
-print "total_list is:",total_list
+print "Bowler list:",total_list
+
+listOfElem1 =[]
+#batsman = driver.find_elements_by_xpath("//*[@id=\"full-scorecard\"]/div[2]/div//table/tbody//tr//td/a[contains(@class, 'playerName')]")
+
+batsmanTotal = 0 
+#import lxml.html
+tree = lxml.html.parse('http://www.espncricinfo.com/indian-premier-league-2016/engine/match/980919.html')
+batting = tree.xpath("//table[@class='batting-table innings']/tr[not(@*)]")
+for player in batting:
+	Batsman={}
+	Batsman['PlayerName'] = player.xpath("./td[@class='batsman-name']/a/text()")[0]
+	runs = player.xpath("./td[4]/text()")[0]
+	balls = player.xpath("./td[6]/text()")[0]
+	fours = player.xpath("./td[7]/text()")[0]
+	sixes = player.xpath("./td[8]/text()")[0]
+	strikeRate = player.xpath("./td[9]/text()")[0]
+
+	
+	batsmanTotal = 0
+	runs = int(runs)
+
+	bonus = math.floor(runs/25)*10
+	if (int(balls) > 0 and runs > 0 ):
+		strikeRate = (float(strikeRate))
+		sixes = (float(sixes))
+		if (runs >=10):
+			if (strikeRate == 0.00 and strikeRate <= 74.99):
+				batsmanTotal -= 15
+			elif (strikeRate > 74.99 and strikeRate <= 99.99):
+				batsmanTotal -= 10
+			elif (strikeRate > 99.9 and strikeRate <=149.99):
+				batsmanTotal += 5
+			elif (strikeRate > 150.00 and strikeRate <=199.99):
+				batsmanTotal += 10
+			else:
+				batsmanTotal += 15
+
+		if (sixes > 0 ):
+			batsmanTotal  += 2*sixes 
+	elif int(runs) == 0:
+		batsmanTotal -= 5
+	batsmanTotal +=runs
+	batsmanTotal +=bonus
+	Batsman['Points'] = batsmanTotal
+
+	#print "total val is: ", total
+	listOfElem1.append(Batsman)
+
+print "Batsman Points:",listOfElem1
